e1da76c1f617290108217890f96cc7cb890d12d5
Priyanshu Gupta <pgupta962000@gmail.com>
2023-06-26 12:08:44 +0530

    add edit get- request

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.post('/login', async (req,res) => {
     const userDoc = await User.findOne({username})
     const passOk= bcrypt.compareSync(password, userDoc.password);
     if(passOk){
-        jwt.sign({username,id:userDoc.id},secret, {} , (err,token) =>{
+        jwt.sign({username,id:userDoc._id},secret, {} , (err,token) =>{
             if(err) throw err;
             res.cookie('token', token).json({
                 id: userDoc._id,
@@ -86,6 +86,35 @@ app.post('/post',uploadMiddleware.single('file'), async (req,res) => {
         res.json(postDoc);
     });
 });
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+    let newPath= null;
+    if(req.file){
+        const {originalname,path} = req.file;
+        const parts = originalname.split('.');
+        const ext= parts[parts.length -1];
+        newPath = path+'.'+ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err,info) =>{
+        if(err) throw err;
+        const{id,title,summary,content}= req.body;
+        const postDoc= await Post.findById(id);
+        const isAuthor =  JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        if(!isAuthor){
+            return res.status(400).json('you are not the author');
+        }
+        await postDoc.updateOne({
+            title,
+            summary,
+            content,
+            cover: newPath ? newPath : postDoc.cover,
+        });
+        res.json(postDoc);
+    });
+})
 
 app.get('/post', async (req,res) => {
     res.json(await Post.find()
